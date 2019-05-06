bd1777b90d62d63aa4609fb2c6de22d67abe929d
akanksha1808 <onlyakanksha95@gmail.com>
2019-05-06 08:41:29 +0530

    linked list loop and its removal

---
+++ loop/LinkedList.java
@@ -0,0 +1,75 @@
+package linkedlist.loop;
+
+public class LinkedList {
+ 
+	Node head;
+	
+	class Node{
+		
+		int data;
+		Node next;
+		
+		Node(int data){
+			this.data=data;
+			this.next=null;
+		}
+		
+	}
+	
+	public void add(int data) {
+		
+		if(head==null)
+			head= new Node(data);
+		else
+		{
+			Node temp=new Node(data);
+			temp.next=head;
+			head=temp;
+		}
+		
+	}
+	
+	public void print()
+	{
+		Node temp=head;
+		while(temp!=null) {
+			System.out.print(temp.data+ " ");
+			temp=temp.next;
+			
+		}
+	}
+	
+	public void loop() {
+		Node fast=head;
+		Node slow=head;
+		
+		while((slow!=null)  && (fast!=null) && (fast.next!=null) ) {
+			slow=slow.next;
+			fast=fast.next.next;
+			if(slow == fast) {
+				System.out.println("loop found");
+		         return;
+			}
+			
+			}
+		return;
+		}
+	
+	
+	public static void main(String... strings)
+	{
+	  
+	  LinkedList list=new LinkedList();
+	  list.add(1);
+	  list.add(2);
+	  list.add(3);
+	  list.add(4);
+	  list.print();
+	  list.head.next.next.next.next=list.head;
+		//list.print();
+		list.loop();
+		
+	}
+	
+	
+}
